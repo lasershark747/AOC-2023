8b3decdb9d20f5fc610a2248ab331d6902fd722a
Mjsta <Mjsta@DESKTOP-L81FVB6>
2023-12-02 06:09:35 +0000

    Add project files.

---
+++ d2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            ulong count = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string num = input[i];
+                ulong red = 0;
+                ulong blue = 0;
+                ulong green = 0;
+                ulong minr = 0;
+                ulong minb = 0;
+                ulong ming = 0;
+                bool succes= true;
+
+                string[] split = num.Split(' ');
+                for(int j = 0; j < split.Length; j++)
+                {
+                    if (split[j][0] == 'b')
+                    {
+                        blue += ulong.Parse(split[j - 1]);
+                    }
+                    if (split[j][0] == 'r')
+                    {
+                        red += ulong.Parse(split[j - 1]);
+                    }
+                    if (split[j][0] == 'g')
+                    {
+                        green += ulong.Parse(split[j - 1]);
+                    }
+                    
+                    
+                    if (split[j][split[j].Length - 1] == ';' || j ==split.Length-1)
+                    {
+                        if(green > ming)
+                        {
+                            ming = green;
+                        }
+                        if(blue> minb )
+                        {
+                            minb= blue;
+                        }
+                        if (red > minr)
+                        {
+                            minr = red;
+                        }
+
+                        red = 0;blue = 0;green = 0;
+                    }
+
+                }
+
+                count +=minr*minb*ming;
+                }
+
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ q1/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            int count = 0;
+
+            string[] strings = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string num = "";
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if (input[i][j] >= 48 && input[i][j] <= 57)
+                    {
+                        num += input[i][j];
+                        break;
+                    }
+                    if (j <= input[i].Length - 3)
+                    {
+                        if (input[i].Substring(j, 3) == "one")
+                        {
+                            num += 1;
+                            break;
+
+                        }
+                        else if (input[i].Substring(j, 3) == "two")
+                        {
+                            num += 2;
+                            break;
+                        }
+
+                        else if (input[i].Substring(j, 3) == "six")
+                        {
+                            num += 6; break;
+
+                        }
+
+                    }
+                    if (j <= input[i].Length - 4)
+                    {
+                        if (input[i].Substring(j, 4) == "four")
+                        {
+                            num += 4;
+                            break;
+                        }
+                        else if (input[i].Substring(j, 4) == "five")
+                        {
+                            num += 5;
+                            break;
+
+                        }
+                        else if (input[i].Substring(j, 4) == "nine")
+                        {
+                            num += 9; break;
+
+                        }
+                    }
+                    if (j <= input[i].Length - 5)
+                    {
+                        if (input[i].Substring(j, 5) == "three")
+                        {
+                            num += 3;
+                            break;
+                        }
+                        else if (input[i].Substring(j, 5) == "seven")
+                        {
+                            num += 7; break;
+
+                        }
+                        else if (input[i].Substring(j, 5) == "eight")
+                        {
+                            num += 8; break;
+
+                        }
+                    }
+                }
+
+
+                for (int j = input[i].Length - 1; j >= 0; j--)
+                {
+                    if (input[i][j] >= 48 && input[i][j] <= 57)
+                    {
+                        num += input[i][j];
+                        break;
+                    }
+                    if (j <= input[i].Length - 3)
+                    {
+                        if (input[i].Substring(j, 3) == "one")
+                        {
+                            num += 1;
+                            break;
+
+                        }
+                        else if (input[i].Substring(j, 3) == "two")
+                        {
+                            num += 2;
+                            break;
+                        }
+
+                        else if (input[i].Substring(j, 3) == "six")
+                        {
+                            num += 6; break;
+
+                        }
+
+                    }
+                    if (j <= input[i].Length - 4)
+                    {
+                        if (input[i].Substring(j, 4) == "four")
+                        {
+                            num += 4;
+                            break;
+                        }
+                        else if (input[i].Substring(j, 4) == "five")
+                        {
+                            num += 5;
+                            break;
+
+                        }
+                        else if (input[i].Substring(j, 4) == "nine")
+                        {
+                            num += 9; break;
+
+                        }
+                    }
+                    if (j <= input[i].Length - 5)
+                    {
+                        if (input[i].Substring(j, 5) == "three")
+                        {
+                            num += 3;
+                            break;
+                        }
+                        else if (input[i].Substring(j, 5) == "seven")
+                        {
+                            num += 7; break;
+
+                        }
+                        else if (input[i].Substring(j, 5) == "eight")
+                        {
+                            num += 8; break;
+
+                        }
+                    }
+                }
+                
+                
+                Console.WriteLine(num);
+                Console.WriteLine(input[i]);
+                count += int.Parse(num);
+
+            }
+
+
+
+            Console.WriteLine(count);
+                Console.ReadKey();
+            }
+        }
+    }
+
