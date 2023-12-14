3d77e816699546229ff4498086aa3d1761edf8b4
Michael Stalley <mjstalley2209@gmail.com>
2023-12-14 15:58:07 +0000

    d13

--- d13/Program.cs
+++ d13/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Security.Principal;
 
 namespace d5
 {
@@ -13,14 +14,120 @@ namespace d5
         {
             string[] input = File.ReadAllLines("input.txt");
             long count = 0;
-
+            int startRow = 0;
+            int endRow = 0;
+            int grids = 0;
+            List<List<string>> list = new List<List<string>>();
             for (int i = 0; i < input.Length; i++)
             {
-
+                if (input[i].Length == 0)
+                {
+                    endRow = i - 1;
+                    List<string> temp = new List<string>();
+                    for (int j = startRow; j <= endRow; j++)
+                    {
+                        temp.Add(input[j]);
+                    }
+                    list.Add(temp);
+                    startRow = i + 1;
+                    grids++;
+                }
             }
 
-            Console.WriteLine(count);
+            foreach (List<string> s in list)
+            {
+                long temp = findSymmetry(s, 0);
+                count += temp;
+            }
+
+            Console.WriteLine("P1: " + count);
+            count = 0;
+            foreach (List<string> s in list)
+            {
+                long temp = findSymmetry(s, 1);
+                count += temp;
+            }
+
+            Console.WriteLine("P2: " + count);
             Console.ReadKey();
         }
+        static long findSymmetry(List<string> s, int diff)
+        {
+            long count = 0;
+            List<string> grid = s;
+            List<string> result = Transpose(grid);
+
+            bool done = false;
+            for (int i = 0; i < grid.Count - 1; i++)
+            {
+                int differences = 0;
+
+                int num = 0;
+
+                while (num + i + 1 < grid.Count && i - num >= 0)
+                {
+                    for (int a = 0; a < grid[i - num].Length; a++)
+                    {
+                        if (grid[i - num][a] != grid[i + num + 1][a])
+                        {
+                            differences++;
+                        }
+                    }
+                    num++;
+                }
+                if (differences == diff)
+                {
+                    count += (i + 1) * 100;
+                    done = true;
+                    break;
+                }
+            }
+
+            if (!done)
+            {
+                for (int i = 0; i < result.Count; i++)
+                {
+                    int differences = 0;
+
+                    int num = 0;
+
+                    while (num + i + 1 < result.Count && i - num >= 0)
+                    {
+                        for (int a = 0; a < result[i - num].Length; a++)
+                        {
+                            if (result[i - num][a] != result[i + num + 1][a])
+                            {
+                                differences++;
+                            }
+                        }
+                        num++;
+                    }
+                    if (differences == diff)
+                    {
+                        count += (i + 1);
+                        done = true;
+                        break;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public static List<string> Transpose(List<string> matrix)
+        {
+            List<string> transposed = new List<string>();
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                string buffer = "";
+                for (int j = 0; j < matrix.Count; j++)
+                {
+                    buffer += matrix[j][i];
+                }
+                transposed.Add(buffer);
+            }
+
+            return transposed;
+        }
+
     }
 }
--- d14/Program.cs
+++ d14/Program.cs
@@ -3,13 +3,153 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
-namespace d14
+namespace d5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string[] input = File.ReadAllLines("input.txt");
+            long count = 0;
+            int moves = 0;
+            List<List<char>> list = new List<List<char>>();
+            List<List<char>> start = new List<List<char>>();
+
+            foreach (string line in input)
+            {
+                List<char> list2 = new List<char>();
+                foreach (char c in line)
+                {
+                    list2.Add(c);
+                }
+                list.Add(list2);
+            }
+            List<string> dict = new List<string>();
+
+            for (int a = 0; a < 3; a++)
+            {
+                if (start == list && a != 0)
+                {
+                    moves = a;
+                    break;
+                }
+                else
+                {
+                    for (int i = 1; i < list.Count; i++)
+                    {
+                        for (int j = 0; j < list[i].Count; j++)
+                        {
+                            if (list[i][j] == 'O')
+                            {
+                                if (list[i - 1][j] == '.')
+                                {
+                                    moves++;
+                                    list[i - 1][j] = 'O';
+                                    list[i][j] = '.';
+                                }
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        for (int j = 1; j < list[i].Count; j++)
+                        {
+                            if (list[i][j] == 'O')
+                            {
+                                if (list[i][j - 1] == '.')
+                                {
+                                    moves++;
+                                    list[i][j - 1] = 'O';
+                                    list[i][j] = '.';
+                                }
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < list.Count - 1; i++)
+                    {
+                        for (int j = 0; j < list[i].Count; j++)
+                        {
+                            if (list[i][j] == 'O')
+                            {
+                                if (list[i + 1][j] == '.')
+                                {
+                                    moves++;
+                                    list[i + 1][j] = 'O';
+                                    list[i][j] = '.';
+                                }
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        for (int j = 0; j < list[i].Count - 1; j++)
+                        {
+                            if (list[i][j] == 'O')
+                            {
+                                if (list[i][j + 1] == '.')
+                                {
+                                    moves++;
+                                    list[i][j + 1] = 'O';
+                                    list[i][j] = '.';
+                                }
+                            }
+                        }
+                    }
+                    string st = "";
+                    foreach (List<char> l in list)
+                    {
+                        string t = "";
+                        foreach (char c in l)
+                        {
+                            t += c;
+                        }
+                        Console.WriteLine(t);
+                    }
+                    Console.WriteLine();
+                }
+
+                if (a % 100000 == 0)
+                {
+                    Console.WriteLine(a);
+                }
+
+            }
+
+            /*
+            string s = dict[1000000000 % moves];
+            List<List<char>> hope = new List<List<char>>();
+            int num = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                List<char> temp = new List<char>();
+                for(int j = 0; j < input[i].Length; j++)
+                {
+                    temp.Add(s[num]);
+                    num++;
+                }
+            }
+
+            for (int i = 0; i < hope.Count; i++)
+            {
+                for (int j = 0; j < hope[i].Count; j++)
+                {
+                    if (hope[i][j] == 'O')
+                    {
+                        count += hope.Count - i;
+                    }
+                }
+            }
+            */
+            Console.WriteLine();
+            Console.WriteLine(count);
+            Console.ReadKey();
         }
     }
 }
+
