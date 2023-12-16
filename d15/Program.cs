e8916ccf3a4975f6cf45e26f7877c18f0262e80b
Mjsta <Mjsta@DESKTOP-L81FVB6>
2023-12-16 18:49:58 +0000

    d15

--- d15/Program.cs
+++ d15/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 
 namespace d5
-{ 
+{
     internal class Program
     {
         static void Main(string[] args)
--- d15/Properties/AssemblyInfo.cs
+++ d15/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("60431458-0df8-428d-9d71-1a0b32ccebd4")]
+[assembly: Guid("4e690138-3428-4f62-88d4-c2b31948351a")]
 
 // Version information for an assembly consists of the following four values:
 //
